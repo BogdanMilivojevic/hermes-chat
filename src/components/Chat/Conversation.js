f3f2d228ce21dc474fcdea2fceaa5e12189c26f1
BogdanMilivojevic <bogdan.milivojeviczmn@gmail.com>
2022-10-11 17:08:29 +0200

    HE-24-Update-Profile

--- src/components/Chat/Conversation.js
+++ src/components/Chat/Conversation.js
@@ -1,20 +1,68 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Messages from './Messages'
 import Input from './Input'
 import { ChatContext } from '../../context/ChatContext'
+import { AuthContext } from '../../context/AuthContext'
+import { doc, onSnapshot } from 'firebase/firestore'
+import { db } from '../../firebase'
+import sound from '../../sound/notification-sound.mp3'
 
 const Conversation = () => {
   const { data } = useContext(ChatContext)
+  const { currentUser } = useContext(AuthContext)
+  const [userData, setUserData] = useState({})
+  const [count, setCount] = useState(1)
 
-  if (data) {
+  // New Message notification
+  const showNotification = () => {
+    if (document.hidden && currentUser.uid) {
+      setCount(count + 1)
+      document.title = `(${count}) Messages`
+      const audio = new Audio(sound)
+      audio.play()
+    } else {
+      console.log('User is on the page')
+    }
+  }
+  useEffect(() => {
+    const getData = () => {
+      const unsub = onSnapshot(doc(db, 'userConversations', currentUser.uid), (doc) => {
+        doc.exists() && setUserData(doc.data())
+      })
+      return () => {
+        unsub()
+      }
+    }
+    currentUser.uid && getData()
+  }, [currentUser.uid])
+
+  useEffect(() => {
+    showNotification()
+  }, [userData])
+
+  // Reseting the document.title
+  document.onvisibilitychange = () => {
+    if (!document.hidden) {
+      setCount(1)
+      document.title = 'Hermes-chat'
+    }
+  }
+
+  if (data.chatId) {
     return (
-      <div className='chat' >
+      <div className='chat'>
         <div className='chat-info'>
           {data.user && <span>{data.user?.displayName}</span>}
           {data.user.photoURL && <img src={data.user.photoURL}/>}
         </div>
         <Messages/>
         <Input/>
+      </div>
+    )
+  } else {
+    return (
+      <div className='chat-hidden'>
+        <h1>Please, select a user to send a message</h1>
       </div>
     )
   }
